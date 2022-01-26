6c2cd20366c2dd5ac1c51737c3d4b03b19179490
Doko-Miles Thorburn <35698678+dthorburn@users.noreply.github.com>
2022-01-26 11:37:29 +0000

    Create NF_Mapping.sh

---
+++ NF_Mapping.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#PBS -lwalltime=72:00:00
+#PBS -lselect=1:ncpus=32:mem=62gb
+#PBS -N Mapping_Submitter_NF
+#PBS -j oe
+
+Project_Dir=/rds/general/user/dthorbur/ephemeral/02_Data/01_Pure_HFA
+cd $Project_Dir
+
+module load nextflow/20.10.0
+
+echo "Starting: `date`"
+nextflow run Mapping.nf -c nextflow.config --profile imperial --fastqc
+echo "Finished: `date`"
