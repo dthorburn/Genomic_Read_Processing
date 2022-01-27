b7f42a552c9a2a3075d066e40ac3d2dacc9872ce
Doko-Miles Thorburn <35698678+dthorburn@users.noreply.github.com>
2022-01-27 16:59:49 +0000

    Update Mapping.sh

--- Mapping.sh
+++ Mapping.sh
@@ -4,11 +4,11 @@
 #PBS -N NF_Mapping_Coordinator
 #PBS -j oe
 
-Project_Dir=/rds/general/user/dthorbur/ephemeral/02_Data/01_Pure_HFA
+Project_Dir=/path/to/project/dir
 cd $Project_Dir
 
 module load nextflow/20.10.0
 
 echo "Starting: `date`"
-nextflow run Mapping.nf -c nextflow.config --profile imperial --fastqc
+nextflow run Mapping.nf -c Mapping.config --profile imperial
 echo "Finished: `date`"
