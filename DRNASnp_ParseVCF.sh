26686841a245fd60a5b2a48e4428a4db70496033
Doko-Miles Thorburn <35698678+dthorburn@users.noreply.github.com>
2022-02-16 09:55:38 +0000

    Update DRNASnp_ParseVCF.sh

--- DRNASnp_ParseVCF.sh
+++ DRNASnp_ParseVCF.sh
@@ -2,7 +2,6 @@
 #PBS -lwalltime=24:00:00
 #PBS -lselect=1:ncpus=4:mem=12gb
 #PBS -N DRNASnp_ParseVCF
-#PBS -J 1-7
 #PBS -j oe
 
 module load anaconda3/personal
@@ -12,7 +11,12 @@ Project_Dir=/path/to/project/dir
 cd $Project_Dir
 
 ls -1 ${Project_Dir}/06_SelectVariants/*SNPs.raw.vcf > ${Project_Dir}/VCF_List.txt
-vcf=`sed ${PBS_ARRAY_INDEX}"q;d" ${Project_Dir}/VCF_List.txt`
 
-Rscript ${Project_Dir}/DRNASnp_ParseVCF.R $vcf
-echo "Done with ${vcf}"
+## 1-7 as there are typically 7 chromosomes we are interested in for A. gambiae
+## NB. If the VCFs are really big, this needs to be converted to an array. 
+for i in {1..7..1}
+do
+	vcf=`sed ${i}"q;d" ${Project_Dir}/VCF_List.txt`
+	Rscript ${Project_Dir}/DRNASnp_ParseVCF.R $vcf
+	echo "Done with ${vcf}"
+done
