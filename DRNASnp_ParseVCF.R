809328ad283551efb94668c5d30a7903e1bc8182
Doko-Miles Thorburn <35698678+dthorburn@users.noreply.github.com>
2022-02-22 16:27:27 +0000

    Update DRNASnp_ParseVCF.R

--- DRNASnp_ParseVCF.R
+++ DRNASnp_ParseVCF.R
@@ -1,5 +1,4 @@
 ## Parse an unphased VCF file for it's annotationsto generate a able/plots to inform hard filtering in GATK
-
 suppressMessages(library(vcfR))
 suppressMessages(library(dplyr))
 suppressMessages(library(ggpubr))
@@ -8,6 +7,7 @@ suppressMessages(library(stringr))
 suppressMessages(library(data.table))
 
 Genome <- commandArgs(trailingOnly=TRUE)[1]
+Mode <- commandArgs(trailingOnly=TRUE)[2]
 fil_name <- Genome %>% gsub(pattern = ".*/", "", .) %>% gsub(pattern = "\\..*", "", .)
 temp_file <- read.vcfR(Genome, verbose = FALSE)
 
@@ -19,9 +19,6 @@ output <- data.table( 	File = fil_name,
 						FS 	= str_extract(pattern = "FS=[0-9]+\\.[0-9]+?", Ann_info) %>% gsub(pattern = "FS=", "", .) %>% as.numeric(),
 						MQ 	= str_extract(pattern = "MQ=[0-9]+\\.[0-9]+?", Ann_info) %>% gsub(pattern = "MQ=", "", .) %>% as.numeric(),
 						SOR = str_extract(pattern = "SOR=[0-9]+\\.[0-9]+?", Ann_info) %>% gsub(pattern = "SOR=", "", .) %>% as.numeric())
-
-#fwrite(file = paste0(Output_path, "/", fil_name, "_Annotation_Distribution.csv"), output)
-
 
 p1 <- ggplot(output, aes(x = QD, fill = File, colour = File)) +
 			geom_density(alpha = 0.2, size = 1) +
@@ -62,4 +59,10 @@ p4 <- ggplot(output, aes(x = SOR, fill = File, colour = File)) +
 
 figure <- ggarrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
 figure
-ggsave(file = paste0("./06_SelectVariants/", fil_name, "_Annotation_Distribution.png"), height = 300, width = 400, units = "mm")
+if(Mode == "DNAseq"){
+	ggsave(file = paste0("./06_SelectVariants/", fil_name, "_Annotation_Distribution.png"), height = 300, width = 400, units = "mm")
+	fwrite(file = paste0("./06_SelectVariants/", fil_name, "_Annotation_Distribution.csv"), output)
+} else if(Mode == "RNAseq"){
+	ggsave(file = paste0("./07_MergedVCFs/", fil_name, "_Annotation_Distribution.png"), height = 300, width = 400, units = "mm")
+	fwrite(file = paste0("./07_MergedVCFs/", fil_name, "_Annotation_Distribution.csv"), output)
+}
