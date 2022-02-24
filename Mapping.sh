f6571470121aa7f8d24cf247dc2764fd301383af
Doko-Miles Thorburn <35698678+dthorburn@users.noreply.github.com>
2022-02-24 17:02:48 +0000

    Update Mapping.sh

--- Mapping.sh
+++ Mapping.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 #PBS -lwalltime=72:00:00
-#PBS -lselect=1:ncpus=32:mem=62gb
+#PBS -lselect=1:ncpus=8:mem=12gb
 #PBS -N NF_Mapping_Coordinator
 #PBS -j oe
 
-Project_Dir="/path/to/project/dir"
+Project_Dir=/path/to/project/dir
 cd $Project_Dir
 
 module load nextflow/20.10.0
 
 echo "Starting: `date`"
-nextflow run Mapping.nf -c Mapping.config --profile imperial
+nextflow run Mapping.nf -c Mapping.config \
+	--profile imperial \
+	--RefGen "/path/to/genome.{fasta,fa,fna}" \
+	--InDir "/path/to/raw_fastq.gzs/" 
 echo "Finished: `date`"
